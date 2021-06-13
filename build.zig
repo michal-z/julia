6437e027ead244f5873f6d079d4f69f07a360b51
Michal Ziulek <michalziulek@gmail.com>
2021-06-13 23:24:41 +0200

    -

--- build.zig
+++ build.zig
@@ -36,7 +36,7 @@ pub fn build(b: *std.build.Builder) void {
 
     exe.addIncludeDir("c");
 
-    // TODO: Remove this once https://github.com/ziglang/zig/issues/8531 is fixed
+    // TODO: Remove this once https://github.com/ziglang/zig/issues/8531 is fixed.
     exe.want_lto = false;
 
     exe.linkSystemLibrary("user32");
